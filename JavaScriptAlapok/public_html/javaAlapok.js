ab2ae7876a6e2f66b45b85a3868525abcfc5c7e6
T. Virág <virmoc411@gmail.com>
2021-03-17 17:35:18 +0100

    négyes feladat

--- JavaScriptAlapok/public_html/javaAlapok.js
+++ JavaScriptAlapok/public_html/javaAlapok.js
@@ -1,43 +1,54 @@
-var tomb=[];
+var tomb = [];
 
-function feltolt(db){
-    tomb=[];
-    
+function feltolt(db) {
+    tomb = [];
+
     for (var i = 0; i < db; i++) {
-        var vel=Math.floor(Math.random()*10+1);
-        tomb[i]=vel;
-        }
-        console.log(tomb);
-        document.getElementById("szoveg").innerHTML="A tomb elemei: "+tomb.join(";");
-}        
-function osszegzes(){        
-    var osszeg=0;
-    for (var i = 0; i < tomb.length; i++) {
-        osszeg+=tomb[i];
+        var vel = Math.floor(Math.random() * 10 + 1);
+        tomb[i] = vel;
     }
-    document.getElementById("osszeg").innerHTML="Az elemek összege: " + osszeg;
+    console.log(tomb);
+    document.getElementById("szoveg").innerHTML = "A tomb elemei: " + tomb.join(";");
+}
+function osszegzes() {
+    var osszeg = 0;
+    for (var i = 0; i < tomb.length; i++) {
+        osszeg += tomb[i];
+    }
+    document.getElementById("osszeg").innerHTML = "Az elemek összege: " + osszeg;
 }
 
-function dolgozz(){
+function otteloszthato() {
+    var oszthat = 0;
+    for (var i = 0; i < tomb.length; i++) {
+        if (tomb[i] % 5 === 0) {
+            oszthat += 1;
+        }
+    }
+    document.getElementById("oszthato").innerHTML = "5-tel oszthatók száma: " + oszthat;
+}
+
+function dolgozz() {
 
     //alert("Jó reggelt!");
     //document.write("Hello");
-    var adat=document.getElementById("adat").value;
+    var adat = document.getElementById("adat").value;
     feltolt(adat);
     osszegzes();
-    
+    otteloszthato();
+
 //    tomb[0]=2;
 //    tomb[1]="Kismacska";
 //    tomb[2]=false;
 //    tomb[3]=5.4;
 //    var tomb = new Arrey(34);
-    
+
 }
 
-function init(){
-console.log("Itt vagyok!");
-document.getElementById("szoveg").innerHTML="A JS dolgozik! ";
-document.getElementById("ok").addEventListener("click",dolgozz);
+function init() {
+    console.log("Itt vagyok!");
+    document.getElementById("szoveg").innerHTML = "A JS dolgozik! ";
+    document.getElementById("ok").addEventListener("click", dolgozz);
 }
 
 window.addEventListener("load", init);
